94ac2a2c24ad7847bb12737cb42aa5204bbf52a8
ashen007 <asheniranga@gmail.com>
2021-07-20 14:31:29 +0530

    record mapping module added

---
+++ National Survey of Family Growth/Modules/mapping_rcd.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+
+
+class MakePregMap:
+    def __init__(self, data, id_column):
+        self.data = data
+        self.case_ids = id_column
+
+    def unique_responds(self):
+        return self.data[self.case_ids].unique()
+
+    def pregnancy_map(self):
+        maped_pregs = {}
+        for case_id in self.unique_responds():
+            maped_pregs[case_id] = self.data[self.data[self.case_ids] == case_id]
+
+        return maped_pregs
+
+
+def get_preg_lengths(data, id_column):
+    preg_len = {}
+    maped_pregs = MakePregMap(data, id_column)
+
+    for key, value in maped_pregs.pregnancy_map().items():
+        preg_len[key] = [value[value['birthord'] == 1]['gest_lb'].values[0],
+                         np.ceil(np.median(value[value['birthord'] > 1]['gest_lb']))]
+
+    return pd.DataFrame(preg_len.values(),
+                        index=preg_len.keys(),
+                        columns=['firstBabies', 'others'])
+
+
+def get_res_ages(data, id_column):
+    resp_ages = {}
+    maped_pregs = MakePregMap(data, id_column)
+
+    for key, value in maped_pregs.pregnancy_map().items():
+        temp = value.sort_values(by='birthord')
+        resp_ages[key] = [temp.head(1)['ager'].values[0],
+                          temp.head(1)['pregordr'].values[0],
+                          temp.head(1)['birthord'].values[0],
+                          temp.head(1)['agecon'].values[0],
+                          temp.head(1)['agepreg'].values[0],
+                          temp.tail(1)['pregordr'].values[0],
+                          temp.tail(1)['birthord'].values[0],
+                          temp.tail(1)['agecon'].values[0],
+                          temp.tail(1)['agepreg'].values[0],
+                          ]
+
+    return pd.DataFrame(resp_ages.values(),
+                        index=resp_ages.keys(),
+                        columns=['age_int',
+                                 'pregord_fst', 'birthord_fst',
+                                 'agecon_fst', 'agepreg_fst',
+                                 'pregord_lst', 'birthord_lst',
+                                 'agecon_lst', 'agepreg_lst'])
